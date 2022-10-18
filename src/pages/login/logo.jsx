1ad5cdff4aa86ce51a9650a5ecc9bda1c9ca7afb
F1rman <ihorkharchyshyn@gmail.com>
2022-10-18 23:02:27 +0300

    first work

---
+++ src/pages/login/logo.jsx
@@ -0,0 +1,11 @@
+
+function Logo() {
+  return (
+      <div className="w-[150px] h-[150px] rounded-full bg-white">
+<div className='text-[#DB4F71] '>IT FUTURE</div>
+<div className='text-[#DB4F71] '>онлайн школа програмування</div>
+</div>
+  );
+}
+
+export default Logo;
